95910b8a949e2e3e42f35e5d64df15232d80e3ab
asil ahmed <hashimchennadan@gmail.com>
2023-12-16 14:12:50 +0530

    intial

---
+++ index.js
@@ -0,0 +1,10 @@
+//go to specific id
+//put the html element inside that id
+
+let element=document.getElementById("sumDemo");
+console.log(element); //checking purpose
+
+//here we inserting the html using innerHTML code
+element.innerHTML= "<p> This was a space for showing the DOM concept</p>";
+
+
